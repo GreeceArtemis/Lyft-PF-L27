f93163846219609c3424c3397033b197b5af9b0c
GreeceArtemis <greece.rapu@gmail.com>
2017-05-02 17:17:49 -0500

    cont terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,3 +30,39 @@ window.addEventListener("scroll",function(){
 
 
 })
+var phoneclick=document.getElementById("phone");
+phoneclick.addEventListener("click",function(){
+  document.getElementsByClassName("moredate")[0].style.display="block";
+  document.getElementsByClassName("datePhone")[0].style.height="410px"
+})
+
+//window.onload=function(){
+  var phone=document.getElementById("phone").value;
+  var name=document.getElementById("name").value;
+  var email=document.getElementById("email").value;
+  var city=document.getElementById("city").value;
+var soloLetras = function(e){
+  var codigoTecla = e.keyCode;
+  if((codigoTecla>=97 && codigoTecla<=122) || (codigoTecla>=65 && codigoTecla<=90)|| codigoTecla==39 || codigoTecla == 32){
+    this.nextElementSibling.nextElementSibling.innerText = "";
+    return true;
+  }else{
+    this.nextElementSibling.nextElementSibling.innerText = "Este campo solo permite letras"
+    return false;
+  }
+}
+var soloNumeros = function(e){
+  var codigoTecla = e.keyCode;
+  var longitud = this.value.length;
+  if(longitud==1){
+    this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.focus();
+  }
+  if(codigoTecla>=48 && codigoTecla<=57 && this.value.length<=2){
+    return true;
+  }else{
+    return false;
+  }
+}
+phone.onkeypress=soloNumeros;
+name.onkeypress=soloLetras;
+city.onkeypress=soloLetras;
