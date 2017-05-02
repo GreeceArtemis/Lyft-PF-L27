7379a74ab5352f6dac19c36c73fe496107704108
GreeceArtemis <greece.rapu@gmail.com>
2017-05-02 15:34:57 -0500

    funcion header terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,20 +3,27 @@ window.addEventListener("scroll",function(){
   var elements=document.getElementsByClassName("element");
 
   if(currentScroll>20){
+    document.getElementsByClassName("logo2")[0].style.display="block";
+    document.getElementsByClassName("logo1")[0].style.display="none";
+    document.getElementsByClassName("divheader")[0].classList.add("fondoheader");
       for(var i=0;i<elements.length;i++){
 
       elements[i].classList.add("show2");
       elements[i].classList.remove("showFirst");
         }
-    document.getElementsByClassName("showFirst2")[0].style="block";
+    document.getElementsByClassName("showFirst2")[0].style.visibility="visible";
     }
   if(currentScroll<20||currentScroll==0){
+    document.getElementsByClassName("divheader")[0].classList.remove("fondoheader");
+    document.getElementsByClassName("logo2")[0].style.display="none";
+    document.getElementsByClassName("logo1")[0].style.display="block";
     for(var i=0;i<elements.length;i++){
 
     elements[i].classList.add("showFirst");
     elements[i].classList.remove("show2");
       }
-  document.getElementsByClassName("showFirst2")[0].style="hidden";
+  document.getElementsByClassName("showFirst2")[0].style.visibility="hidden";
+
 
 
   }
